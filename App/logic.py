fc6573fed9ab5fc9d29562aaf7fe6a3854f03006
afrodriguezm1 <af.rodriguezm1@uniandes.edu.co>
2024-08-29 15:22:58 -0500

    Reto template innit

---
+++ App/logic.py
@@ -0,0 +1,107 @@
+import time
+
+def new_logic():
+    """
+    Crea el catalogo para almacenar las estructuras de datos
+    """
+    #TODO: Llama a las funciónes de creación de las estructuras de datos
+    pass
+
+
+# Funciones para la carga de datos
+
+def load_data(catalog, filename):
+    """
+    Carga los datos del reto
+    """
+    # TODO: Realizar la carga de datos
+    pass
+
+# Funciones de consulta sobre el catálogo
+
+def get_data(catalog, id):
+    """
+    Retorna un dato por su ID.
+    """
+    #TODO: Consulta en las Llamar la función del modelo para obtener un dato
+    pass
+
+
+def req_1(catalog):
+    """
+    Retorna el resultado del requerimiento 1
+    """
+    # TODO: Modificar el requerimiento 1
+    pass
+
+
+def req_2(catalog):
+    """
+    Retorna el resultado del requerimiento 2
+    """
+    # TODO: Modificar el requerimiento 2
+    pass
+
+
+def req_3(catalog):
+    """
+    Retorna el resultado del requerimiento 3
+    """
+    # TODO: Modificar el requerimiento 3
+    pass
+
+
+def req_4(catalog):
+    """
+    Retorna el resultado del requerimiento 4
+    """
+    # TODO: Modificar el requerimiento 4
+    pass
+
+
+def req_5(catalog):
+    """
+    Retorna el resultado del requerimiento 5
+    """
+    # TODO: Modificar el requerimiento 5
+    pass
+
+def req_6(catalog):
+    """
+    Retorna el resultado del requerimiento 6
+    """
+    # TODO: Modificar el requerimiento 6
+    pass
+
+
+def req_7(catalog):
+    """
+    Retorna el resultado del requerimiento 7
+    """
+    # TODO: Modificar el requerimiento 7
+    pass
+
+
+def req_8(catalog):
+    """
+    Retorna el resultado del requerimiento 8
+    """
+    # TODO: Modificar el requerimiento 8
+    pass
+
+
+# Funciones para medir tiempos de ejecucion
+
+def get_time():
+    """
+    devuelve el instante tiempo de procesamiento en milisegundos
+    """
+    return float(time.perf_counter()*1000)
+
+
+def delta_time(start, end):
+    """
+    devuelve la diferencia entre tiempos de procesamiento muestreados
+    """
+    elapsed = float(end - start)
+    return elapsed
---
+++ App/view.py
@@ -0,0 +1,148 @@
+import sys
+
+
+def new_logic():
+    """
+        Se crea una instancia del controlador
+    """
+    #TODO: Llamar la función de la lógica donde se crean las estructuras de datos
+    pass
+
+def print_menu():
+    print("Bienvenido")
+    print("1- Cargar información")
+    print("2- Ejecutar Requerimiento 1")
+    print("3- Ejecutar Requerimiento 2")
+    print("4- Ejecutar Requerimiento 3")
+    print("5- Ejecutar Requerimiento 4")
+    print("6- Ejecutar Requerimiento 5")
+    print("7- Ejecutar Requerimiento 6")
+    print("8- Ejecutar Requerimiento 7")
+    print("9- Ejecutar Requerimiento 8 (Bono)")
+    print("0- Salir")
+
+def load_data(control):
+    """
+    Carga los datos
+    """
+    #TODO: Realizar la carga de datos
+    pass
+
+
+def print_data(control, id):
+    """
+        Función que imprime un dato dado su ID
+    """
+    #TODO: Realizar la función para imprimir un elemento
+    pass
+
+def print_req_1(control):
+    """
+        Función que imprime la solución del Requerimiento 1 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 1
+    pass
+
+
+def print_req_2(control):
+    """
+        Función que imprime la solución del Requerimiento 2 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 2
+    pass
+
+
+def print_req_3(control):
+    """
+        Función que imprime la solución del Requerimiento 3 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 3
+    pass
+
+
+def print_req_4(control):
+    """
+        Función que imprime la solución del Requerimiento 4 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 4
+    pass
+
+
+def print_req_5(control):
+    """
+        Función que imprime la solución del Requerimiento 5 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 5
+    pass
+
+
+def print_req_6(control):
+    """
+        Función que imprime la solución del Requerimiento 6 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 6
+    pass
+
+
+def print_req_7(control):
+    """
+        Función que imprime la solución del Requerimiento 7 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 7
+    pass
+
+
+def print_req_8(control):
+    """
+        Función que imprime la solución del Requerimiento 8 en consola
+    """
+    # TODO: Imprimir el resultado del requerimiento 8
+    pass
+
+
+# Se crea la lógica asociado a la vista
+control = new_logic()
+
+# main del ejercicio
+def main():
+    """
+    Menu principal
+    """
+    working = True
+    #ciclo del menu
+    while working:
+        print_menu()
+        inputs = input('Seleccione una opción para continuar\n')
+        if int(inputs) == 1:
+            print("Cargando información de los archivos ....\n")
+            data = load_data(control)
+        elif int(inputs) == 2:
+            print_req_1(control)
+
+        elif int(inputs) == 3:
+            print_req_2(control)
+
+        elif int(inputs) == 4:
+            print_req_3(control)
+
+        elif int(inputs) == 5:
+            print_req_4(control)
+
+        elif int(inputs) == 6:
+            print_req_5(control)
+
+        elif int(inputs) == 7:
+            print_req_6(control)
+
+        elif int(inputs) == 8:
+            print_req_7(control)
+
+        elif int(inputs) == 9:
+            print_req_8(control)
+
+        elif int(inputs) == 0:
+            working = False
+            print("\nGracias por utilizar el programa") 
+        else:
+            print("Opción errónea, vuelva a elegir.\n")
+    sys.exit(0)
